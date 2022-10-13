33097d2b1425287e077cc79bfe82097744046f19
기현 <79272189+Edwin9905@users.noreply.github.com>
2022-10-13 20:18:53 +0900

    [Silver II] Title: 에디터, Time: 416 ms, Memory: 36572 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -7,12 +7,12 @@ for _ in range(int(sys.stdin.readline())):
         if st1:
             st2.append(st1.pop())
     elif command[0] == 'D':
-    	if st2:
+        if st2:
             st1.append(st2.pop())
     elif command[0] == 'B':
-    	if st1:
+        if st1:
             st1.pop()
     else:
-    	st1.append(command[1])
+        st1.append(command[1])
 st1.extend(reversed(st2))
 print(''.join(st1))
